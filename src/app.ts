8aac9432031478dd3f6130176bf79f07bd2152f4
Helder Costa <heldercostaaa@gmail.com>
2024-07-09 15:55:47 -0300

    feat(email): send confirmation email to trip owner; Add confirmation route to api

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,20 @@
+import cors from "@fastify/cors";
 import fastify from "fastify";
 import {
   serializerCompiler,
   validatorCompiler,
 } from "fastify-type-provider-zod";
+import { confirmTrip } from "./routes/confirm-trip";
 import { createTrip } from "./routes/create-trip";
 
 export const app = fastify();
+
+app.register(cors, {
+  origin: "*",
+});
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
 app.register(createTrip);
+app.register(confirmTrip);
--- src/routes/create-trip.ts
+++ src/routes/create-trip.ts
@@ -1,8 +1,13 @@
 import dayjs from "dayjs";
+import localizedFormat from "dayjs/plugin/localizedFormat";
 import { FastifyInstance } from "fastify";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
+import nodemailer from "nodemailer";
 import { z } from "zod";
+import { getMailClient } from "../lib/mail";
 import { prisma } from "../lib/prisma";
+
+dayjs.extend(localizedFormat);
 
 export async function createTrip(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().post(
@@ -57,6 +62,40 @@ export async function createTrip(app: FastifyInstance) {
           },
         },
       });
+
+      const formattedStartDate = dayjs(startsAt).format("LL");
+      const formattedEndDate = dayjs(endsAt).format("LL");
+      const confirmationUrl = `http://localhost:3333/trips/${trip.id}/confirm`;
+
+      const mail = await getMailClient();
+      const message = await mail.sendMail({
+        from: {
+          name: "Plann.er Team",
+          address: "hello@plann.er",
+        },
+        to: {
+          name: ownerName,
+          address: ownerEmail,
+        },
+        subject: `Confirm you trip to ${destination} on ${formattedStartDate}`,
+        html: `
+        <div style="font-family: sans-serif; font-size: 16px; line-height: 1.6;">
+          <p>You requested a trip creation to <strong>${destination}</strong> between <strong>${formattedStartDate}</strong> and <strong>${formattedEndDate}</strong>.</p>
+          <p></p>
+          <p>To confirm and plan your trip, click the link below:</p>
+          <p></p>
+          <p>
+            <a href="${confirmationUrl} target="_blank">Confirm trip</a>
+          </p>
+          <p></p>
+          <p>If you don't know what this email is about, please disconsider this message.</p>
+        </div>
+        `.trim(),
+      });
+
+      console.log(
+        `Email preview link: ${nodemailer.getTestMessageUrl(message)}`
+      );
 
       return reply.status(201).send({ id: trip.id });
     }
