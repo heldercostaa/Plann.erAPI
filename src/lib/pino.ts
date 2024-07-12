4b792bb78771f3c8d39ae0c6c16829f61ce4519d
Helder Costa <heldercostaaa@gmail.com>
2024-07-12 17:58:41 -0300

    chore: remove supports-color

--- src/lib/pino.ts
+++ src/lib/pino.ts
@@ -1,20 +1,16 @@
 import pino from "pino";
-import { PrettyOptions, isColorSupported } from "pino-pretty";
-import { env } from "../env";
-// @ts-expect-error
-import supportsColor from "supports-color";
+import { PrettyOptions } from "pino-pretty";
+import { env } from "../env/index.js";
 
 const pinoPrettyOptions: PrettyOptions = {
   colorize: true,
   ignore: "pid,hostname",
 };
 
-const pinoPretty = {
-  target: "pino-pretty",
-  options: pinoPrettyOptions,
-};
-
 export const logger = pino({
   level: env.LOG_LEVEL,
-  transport: supportsColor.stdout ? pinoPretty : undefined,
+  transport: {
+    target: "pino-pretty",
+    options: pinoPrettyOptions,
+  },
 });
