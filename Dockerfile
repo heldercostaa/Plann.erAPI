55b6dcfcb2a0ba083537989365074d90818b9b0d
Helder Costa <heldercostaaa@gmail.com>
2024-07-13 22:29:01 -0300

    ci: refactor gh ci; refactor db .env variables

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,6 @@ WORKDIR /app
 
 COPY package.json .
 COPY package-lock.json .
-COPY *.env .
 
 RUN npm install
 
@@ -21,7 +20,6 @@ COPY --from=builder /app/dist/ ./dist
 COPY --from=builder /app/node_modules/ ./node_modules
 COPY --from=builder /app/prisma/migrations/ ./prisma/migrations
 
-COPY --from=builder /app/*.env .
 COPY --from=builder /app/package.json .
 COPY --from=builder /app/prisma/schema.prisma ./prisma/
 
--- src/env/index.ts
+++ src/env/index.ts
@@ -9,19 +9,14 @@ if (process.env.NODE_ENV === "test") {
 
 const envSchema = z.object({
   NODE_ENV: z.enum(["development", "test", "production"]).default("production"),
-  DATABASE_URL: z.string().url(),
+  DB_URL: z.string().url(),
+  DB_LOG_LEVEL: z.enum(["query", "info", "warn", "error"]).default("info"),
   API_BASE_URL: z.string().url(),
   WEB_BASE_URL: z.string().url(),
   PORT: z.coerce.number().default(3333),
   LOG_LEVEL: z
     .enum(["silent", "trace", "debug", "info", "warn", "error", "fatal"])
     .default("info"),
-
-  DB_NAME: z.string(),
-  DB_USER: z.string(),
-  DB_PASSWORD: z.string(),
-  DB_PORT: z.string(),
-  DB_LOG_LEVEL: z.enum(["query", "info", "warn", "error"]).default("info"),
 });
 
 const _env = envSchema.safeParse(process.env);
